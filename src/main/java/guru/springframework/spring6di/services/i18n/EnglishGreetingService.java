a2781258de174d86f1ed021d35ecef99a15d2330
My Name <me@gm.com>
2025-01-27 15:44:27 +0100

    3.39-spring-profiles : assign distinct profile for each service

--- src/main/java/guru/springframework/spring6di/services/i18n/EnglishGreetingService.java
+++ src/main/java/guru/springframework/spring6di/services/i18n/EnglishGreetingService.java
@@ -1,8 +1,10 @@
 package guru.springframework.spring6di.services.i18n;
 
 import guru.springframework.spring6di.services.GreetingService;
+import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
+@Profile("EN")
 @Service("i18Nservice")
 public class EnglishGreetingService implements GreetingService {
 
--- src/main/java/guru/springframework/spring6di/services/i18n/SpanishGreetingService.java
+++ src/main/java/guru/springframework/spring6di/services/i18n/SpanishGreetingService.java
@@ -1,8 +1,10 @@
 package guru.springframework.spring6di.services.i18n;
 
 import guru.springframework.spring6di.services.GreetingService;
+import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
+@Profile("ES")
 @Service("i18Nservice")
 public class SpanishGreetingService implements GreetingService {
 
