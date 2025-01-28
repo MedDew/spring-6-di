8e9757673524ae87c4cf35f72bbc14aee1ac549d
My Name <me@gm.com>
2025-01-28 09:20:53 +0100

    3.40-default-profile : comment the ActiProfile to simulate the use of the default profile loaded

--- src/test/java/guru/springframework/spring6di/controllers/i18n/myi18NControllerTestES.java
+++ src/test/java/guru/springframework/spring6di/controllers/i18n/myi18NControllerTestES.java
@@ -7,7 +7,7 @@ import org.springframework.test.context.ActiveProfiles;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-@ActiveProfiles("ES")
+//@ActiveProfiles("ES")
 @SpringBootTest
 class myi18NControllerTestES {
     @Autowired
